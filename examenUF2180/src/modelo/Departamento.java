cd64b4036204ff1eb6b61568ee2a94173d9653d1
Babsrm <babsruizm@gmail.com>
2023-03-31 18:15:28 +0200

    añadida documentación paquete modelo

--- examenUF2180/src/modelo/Departamento.java
+++ examenUF2180/src/modelo/Departamento.java
@@ -6,7 +6,7 @@ import java.util.Objects;
  * Clase que modela un departamento. Servirá para representar un departamento
  * de la tabla departamentos.
  * 
- * @author Barbara
+ * @author Barbara Ruiz
  *
  */
 
@@ -63,17 +63,30 @@ public class Departamento {
 		this.codDepartamento = codDepartamento;
 	}
 	
+	/**
+	 * Getter del código del centro
+	 * @return int
+	 */
 	public int getCodCentro() {
 		return codCentro;
 	}
 
+	/**
+	 * Establece el código de centro
+	 * @param codCentro int con el código del centro
+	 */
 	public void setCodCentro(int codCentro) {
 		this.codCentro = codCentro;
 	}
 
+	/**
+	 * Getter del tipo de dirección
+	 * @return String con los valores 'P' o 'F'.
+	 */
 	public String getTipoDir() {
 		return tipoDir;
 	}
+	
 	/**
 	 * Método que devuelve la representación en String del tipo de dirección.
 	 * La almacenada en la base de datos solo puede ser char(1)- P o F.
@@ -87,22 +100,42 @@ public class Departamento {
 		}
 	}
 
+	/**
+	 * Establece el tipo de dirección.
+	 * @param tipoDir String con los valores 'P' o 'F'. Deben de indicarse exclusivamente uno de esos dos caracteres porque sino no podrá hacerse el cambio en la base de datos.
+	 */
 	public void setTipoDir(String tipoDir) {
 		this.tipoDir = tipoDir;
 	}
 
+	/**
+	 * Getter que obtiene el presupuesto.
+	 * @return presupuesto int.
+	 */
 	public int getPresupuesto() {
 		return presupuesto;
 	}
 
+	/**
+	 * Método para establecer un presupuesto.
+	 * @param presupuesto int
+	 */
 	public void setPresupuesto(int presupuesto) {
 		this.presupuesto = presupuesto;
 	}
 
+	/**
+	 * Getter que obtiene el nombre del departamento.
+	 * @return nombre String.
+	 */
 	public String getNombre() {
 		return nombre;
 	}
 
+	/**
+	 * Método que establece el nombre del departamento.
+	 * @param nombre String.
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
@@ -112,7 +145,9 @@ public class Departamento {
 		return Objects.hash(codDepartamento);
 	}
 
-	//la clave primaria es codDepartamento por lo que dos departamentos solo serán iguales si tienen el mismo codDepartamento.
+	/**
+	 * Método que indica si dos objetos de la clase {@link Departamento} son iguales. Dos departamentos solo serán iguales si tienen la misma clave primaria (codDepartamento).
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -125,6 +160,9 @@ public class Departamento {
 		return codDepartamento == other.codDepartamento;
 	}
 
+	/**
+	 * Método que pasa a cadena de caracteres el objeto {@link Departamento}.
+	 */
 	@Override
 	public String toString() {
 		return "codDepartamento: " + codDepartamento + ", codCentro: " + codCentro + 
